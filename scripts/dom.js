9e7d87a241f594c3ad101e8a08108d7a651df885
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-10 19:01:16 +0200

    Added some code in boardPlacer() which updates the HTML's .player-name with the Player name

--- scripts/dom.js
+++ scripts/dom.js
@@ -16,26 +16,32 @@ playerTwo.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
 playerTwo.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
 playerTwo.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);
 
+// Need to be moved to a single module
 function boardPlacer(player) {
 	let playerDiv = document.querySelector("." + player.playerNumber);
-    let playerBoard = document.querySelector("." + player.playerNumber + " .gameboard");
-    console.log(playerBoard);
+	let playerBoard = document.querySelector("." + player.playerNumber + " .gameboard");
+	let playerName = document.querySelector("." + player.playerNumber + " .player-informations" + " .player-name");
+	console.log(playerName);
 
-    for (let i = 0; i < 10; i++) {
-        for (let i2 = 0; i2 < 10; i2++) {
-            //if (player.board.board[i][i2] !== "nothing" && player.board.board[i][i2] !== 0) {
-                let square = document.createElement("div");
-                square.classList.add("square");
-                square.textContent = "0";
-                playerBoard.appendChild(square);
-            /*} else {
+	for (let i = 0; i < 10; i++) {
+		for (let i2 = 0; i2 < 10; i2++) {
+			//if (player.board.board[i][i2] !== "nothing" && player.board.board[i][i2] !== 0) {
+			let square = document.createElement("div");
+			square.classList.add("square");
+			square.textContent = "0";
+			playerBoard.appendChild(square);
+			/*} else {
                 let square = document.createElement("div");
                 square.classList.add("square");
                 square.textContent = "0";
                 playerBoard.appendChild(square);
             }*/
-        }
-    }
+
+			// event listener for click
+		}
+	}
+
+	playerName.textContent = String(player.playerName);
 }
 
 boardPlacer(playerOne);
