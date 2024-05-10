8660b2171e4b33de744524b0f028c98aee646c52
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-10 18:41:55 +0200

    Reworked boardPlacer() which now places correctly the board. Added .square which is used for the single squares in the gameboard

--- scripts/dom.js
+++ scripts/dom.js
@@ -11,15 +11,31 @@ playerOne.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
 playerOne.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);
 playerOne.board.placeShip(new Ship(2, 0, false), [4, 3], [5, 3]);
 
-playerOne.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
-playerOne.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
-playerOne.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
-playerOne.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);
-playerOne.board.placeShip(new Ship(2, 0, false), [4, 3], [5, 3]);
+playerTwo.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
+playerTwo.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
+playerTwo.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
+playerTwo.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);
 
 function boardPlacer(player) {
-	let temp = document.querySelector("." + player.playerNumber);
-    console.log(temp);
+	let playerDiv = document.querySelector("." + player.playerNumber);
+    let playerBoard = document.querySelector("." + player.playerNumber + " .gameboard");
+    console.log(playerBoard);
+
+    for (let i = 0; i < 10; i++) {
+        for (let i2 = 0; i2 < 10; i2++) {
+            //if (player.board.board[i][i2] !== "nothing" && player.board.board[i][i2] !== 0) {
+                let square = document.createElement("div");
+                square.classList.add("square");
+                square.textContent = "0";
+                playerBoard.appendChild(square);
+            /*} else {
+                let square = document.createElement("div");
+                square.classList.add("square");
+                square.textContent = "0";
+                playerBoard.appendChild(square);
+            }*/
+        }
+    }
 }
 
 boardPlacer(playerOne);
