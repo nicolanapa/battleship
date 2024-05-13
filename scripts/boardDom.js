9f7dbce021ae59c92afc10e13f9ce6385dec91a6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-13 19:58:14 +0200

    Exported turn() and turnDisabler() to turnDom.jsand imported them from boardDom.js

--- scripts/boardDom.js
+++ scripts/boardDom.js
@@ -1,3 +1,5 @@
+import { turn, turnDisabler } from "./turnDom.js";
+
 function boardPlacer(player) {
 	let playerDiv = document.querySelector("." + player.playerNumber);
 	let playerBoard = document.querySelector("." + player.playerNumber + " .gameboard");
@@ -30,36 +32,6 @@ function boardPlacer(player) {
 	}
 
 	playerName.textContent = String(player.playerName);
-}
-
-function turn(player, playerBoard) {
-	let playerOneBoard = document.querySelector(".main-gameboard .player-one .gameboard");
-	let playerTwoBoard = document.querySelector(".main-gameboard .player-two .gameboard");
-
-	if (player.playerNumber === "player-one") {
-		playerBoard.classList.add("blur");
-		playerTwoBoard.classList.remove("blur");
-	} else if (player.playerNumber === "player-two") {
-		playerOneBoard.classList.remove("blur");
-		playerBoard.classList.add("blur");
-	}
-}
-
-function turnDisabler(player, playerWinStatus) {
-	let playerOneBoard = document.querySelector(".main-gameboard .player-one .gameboard");
-	let playerTwoBoard = document.querySelector(".main-gameboard .player-two .gameboard");
-
-	playerOneBoard.classList.remove("blur");
-	playerTwoBoard.classList.remove("blur");
-	playerOneBoard.classList.add("disabled");
-	playerTwoBoard.classList.add("disabled");
-
-	playerWinStatus.textContent = "You Lose! All your ships have been sunked";
-	if (player.playerNumber === "player-one") {
-		document.querySelector(".player-two .player-informations .win-status").textContent = "You Win! All Enemy's ships have been sunked";
-	} else if (player.playerNumber === "player-two") {
-		document.querySelector(".player-one .player-informations .win-status").textContent = "You Win! All Enemy's ships have been sunked";
-	}
 }
 
 export { boardPlacer };
---
+++ scripts/turnDom.js
@@ -0,0 +1,31 @@
+function turn(player, playerBoard) {
+	let playerOneBoard = document.querySelector(".main-gameboard .player-one .gameboard");
+	let playerTwoBoard = document.querySelector(".main-gameboard .player-two .gameboard");
+
+	if (player.playerNumber === "player-one") {
+		playerBoard.classList.add("blur");
+		playerTwoBoard.classList.remove("blur");
+	} else if (player.playerNumber === "player-two") {
+		playerOneBoard.classList.remove("blur");
+		playerBoard.classList.add("blur");
+	}
+}
+
+function turnDisabler(player, playerWinStatus) {
+	let playerOneBoard = document.querySelector(".main-gameboard .player-one .gameboard");
+	let playerTwoBoard = document.querySelector(".main-gameboard .player-two .gameboard");
+
+	playerOneBoard.classList.remove("blur");
+	playerTwoBoard.classList.remove("blur");
+	playerOneBoard.classList.add("disabled");
+	playerTwoBoard.classList.add("disabled");
+
+	playerWinStatus.textContent = "You Lose! All your ships have been sunked";
+	if (player.playerNumber === "player-one") {
+		document.querySelector(".player-two .player-informations .win-status").textContent = "You Win! All Enemy's ships have been sunked";
+	} else if (player.playerNumber === "player-two") {
+		document.querySelector(".player-one .player-informations .win-status").textContent = "You Win! All Enemy's ships have been sunked";
+	}
+}
+
+export { turn, turnDisabler };
