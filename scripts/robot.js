fcb2378a3fbd4341f93187cb3d19212987713407
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-13 21:03:23 +0200

    Hardcoded return value to test correctly if it works in the tests

--- scripts/robot.js
+++ scripts/robot.js
@@ -1,6 +1,7 @@
 function randomMove() {
 	// it should just return a pair of coordinate, then it will attack i
 	// or from this function or from boardDom / turnDom
+	return [0, 0];
 }
 
 export { randomMove };
