6e605a53bfbb24805f3636ea593d3c8e815ecbec
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 14:49:49 +0200

    Fixed randomMove() which now calculates correctly which square to choose and click

--- scripts/robot.js
+++ scripts/robot.js
@@ -5,8 +5,8 @@ function randomMove() {
 		let move = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
 		let allSquares = document.querySelectorAll(".main-gameboard .player-one .gameboard .square");
 		console.log(move);
-		console.log(allSquares[move[0], move[1]]);
-		allSquares[move[0], move[1]].click();
+		console.log(allSquares[move[0] * 10 + move[1]]);
+		allSquares[move[0] * 10 + move[1]].click();
 		return move;
 	}, 1000);
 }
