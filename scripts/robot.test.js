24f103d905bbfb164e670003bfce7ee81edd7aee
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 14:13:07 +0200

    Added a test in robot.test.js to test random attacking

--- scripts/robot.test.js
+++ scripts/robot.test.js
@@ -16,6 +16,21 @@ test("Testing a random move", () => {
 
 	expect(move[0]).toBeGreaterThanOrEqual(0);
 	expect(move[0]).toBeLessThanOrEqual(9);
-    expect(move[1]).toBeGreaterThanOrEqual(0);
+	expect(move[1]).toBeGreaterThanOrEqual(0);
 	expect(move[1]).toBeLessThanOrEqual(9);
+});
+
+test("Testing attacks", () => {
+	let playerOne = new Player("Luca", "player-one", false);
+	let robot = new Player("User", "player-two", true);
+	playerOne.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
+	playerOne.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
+	playerOne.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
+
+	while (playerOne.board.allSunked !== true) {
+		let move = randomMove();
+		playerOne.board.receiveAttack(move);
+	}
+
+	expect(playerOne.board).toHaveProperty("allSunked", true);
 });
