878434318b09b9f2ecac70dd7656700553852907
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 15:18:31 +0200

    Commented console.log in randomMove() and a value

--- scripts/robot.js
+++ scripts/robot.js
@@ -1,14 +1,12 @@
 function randomMove() {
-	// it should just return a pair of coordinate, then it will attack i
-	// or from this function or from boardDom / turnDom
 	setTimeout(() => {
 		let move = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
 		let allSquares = document.querySelectorAll(".main-gameboard .player-one .gameboard .square");
-		console.log(move);
-		console.log(allSquares[move[0] * 10 + move[1]]);
+		//console.log(move);
+		//console.log(allSquares[move[0] * 10 + move[1]]);
 		allSquares[move[0] * 10 + move[1]].click();
 		return move;
-	}, 1000);
+	}, 500/*1000*/);
 }
 
 export { randomMove };
