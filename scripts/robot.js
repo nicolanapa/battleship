32404080ae9e00e0114d08ed86f88705964431f4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 20:06:43 +0200

    Added a while that tries to do a unique move in randomMove()

--- scripts/robot.js
+++ scripts/robot.js
@@ -2,11 +2,32 @@ function randomMove() {
 	setTimeout(() => {
 		let move = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
 		let allSquares = document.querySelectorAll(".main-gameboard .player-one .gameboard .square");
-		//console.log(move);
+		let tempSquareStatus = allSquares[move[0] * 10 + move[1]];
+		console.log(move);
 		//console.log(allSquares[move[0] * 10 + move[1]]);
-		allSquares[move[0] * 10 + move[1]].click();
+
+		let uniqueMove = 0;
+		while (uniqueMove === 0) {
+			if (allSquares[move[0] * 10 + move[1]].textContent.includes("0")) {
+				allSquares[move[0] * 10 + move[1]].click();
+				uniqueMove = 1;
+			} else if (
+				!allSquares[move[0] * 10 + move[1]].textContent.includes("") &&
+				!allSquares[move[0] * 10 + move[1]].textContent.includes("1")
+			) {
+				allSquares[move[0] * 10 + move[1]].click();
+				uniqueMove = 1;
+			} else if (
+				allSquares[move[0] * 10 + move[1]].textContent.includes("") ||
+				allSquares[move[0] * 10 + move[1]].textContent.includes("1")
+			) {
+				move = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+				uniqueMove = 0;
+			}
+		}
+
 		return move;
-	}, 500/*1000*/);
+	}, 1000);
 }
 
 export { randomMove };
