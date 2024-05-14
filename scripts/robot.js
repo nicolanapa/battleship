83bcf7e3f90b72c27572d9a489788cfe6cf01394
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 14:45:06 +0200

    Added setTimeout() which waits for a second before playing in randomMove(). Added some code which should click a random square but the first index won't work

--- scripts/robot.js
+++ scripts/robot.js
@@ -1,7 +1,14 @@
 function randomMove() {
 	// it should just return a pair of coordinate, then it will attack i
 	// or from this function or from boardDom / turnDom
-	return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+	setTimeout(() => {
+		let move = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+		let allSquares = document.querySelectorAll(".main-gameboard .player-one .gameboard .square");
+		console.log(move);
+		console.log(allSquares[move[0], move[1]]);
+		allSquares[move[0], move[1]].click();
+		return move;
+	}, 1000);
 }
 
 export { randomMove };
