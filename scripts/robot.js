ebb411ed8ca28cecfba8b4510c8c8164720a0bf5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-14 14:13:54 +0200

    Added a random number generator in randomMove()

--- scripts/robot.js
+++ scripts/robot.js
@@ -1,7 +1,7 @@
 function randomMove() {
 	// it should just return a pair of coordinate, then it will attack i
 	// or from this function or from boardDom / turnDom
-	return [0, 0];
+	return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
 }
 
 export { randomMove };
