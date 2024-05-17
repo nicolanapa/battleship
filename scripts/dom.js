109a79790001425b1e59609735d322f2cdc5d12f
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-17 16:15:12 +0200

    Commented all hard-coded Ships

--- scripts/dom.js
+++ scripts/dom.js
@@ -8,14 +8,14 @@ let playerOne = new Player("User" + Math.floor(Math.random() * 10000), "player-o
 //let playerTwo = new Player("Moro", "player-two", false);
 let playerTwo = new Player("User" + Math.floor(Math.random() * 10000), "player-two", true);
 
-playerOne.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
+/*playerOne.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
 playerOne.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
 playerOne.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
 playerOne.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);
 playerOne.board.placeShip(new Ship(2, 0, false), [4, 3], [5, 3]);
 
 playerTwo.board.placeShip(new Ship(5, 0, false), [2, 3], [2, 7]);
-/*playerTwo.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
+playerTwo.board.placeShip(new Ship(3, 0, false), [3, 1], [5, 1]);
 playerTwo.board.placeShip(new Ship(2, 0, false), [1, 0], [1, 1]);
 playerTwo.board.placeShip(new Ship(4, 0, false), [6, 6], [6, 9]);*/
 
